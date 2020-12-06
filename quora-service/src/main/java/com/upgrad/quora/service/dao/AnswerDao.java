f066e87ab9212e599aae056ab044a74b892261cf
Kavya-Gowtham <kavyanagaraj20596@gmail.com>
2020-12-06 20:32:18 +0530

    Added Edit Answer Feature

--- quora-service/src/main/java/com/upgrad/quora/service/dao/AnswerDao.java
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/AnswerDao.java
@@ -4,6 +4,7 @@ import com.upgrad.quora.service.entity.AnswerEntity;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 
 @Repository
@@ -20,5 +21,29 @@ public class AnswerDao {
     public AnswerEntity createAnswer(AnswerEntity answerEntity) {
         entityManager.persist(answerEntity);
         return answerEntity;
+    }
+    /**
+     * get answers from the DB
+     *
+     * @param answerId : require answerid
+     */
+    public AnswerEntity getAnswerById(final String answerId) {
+        try {
+            return entityManager
+                    .createNamedQuery("getAnswerById", AnswerEntity.class)
+                    .setParameter("uuid", answerId)
+                    .getSingleResult();
+        } catch (NoResultException nre) {
+            return null;
+        }
+    }
+
+    /**
+     * Update answers in the DB
+     *
+     * @param answerEntity : require answer body
+     */
+    public void updateAnswer(AnswerEntity answerEntity) {
+        entityManager.merge(answerEntity);
     }
 }
