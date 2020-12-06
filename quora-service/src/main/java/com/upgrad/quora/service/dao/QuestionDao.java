97c117c5c9f21680134eaf596639f17568ded9a9
Kavya-Gowtham <kavyanagaraj20596@gmail.com>
2020-12-06 17:28:08 +0530

    GetAllQuestions Feature Added

--- quora-service/src/main/java/com/upgrad/quora/service/dao/QuestionDao.java
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/QuestionDao.java
@@ -1,11 +1,9 @@
 package com.upgrad.quora.service.dao;
 
 import com.upgrad.quora.service.entity.QuestionEntity;
-import com.upgrad.quora.service.entity.UserEntity;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
@@ -24,5 +22,16 @@ public class QuestionDao {
     public QuestionEntity createQuestion(QuestionEntity questionEntity) {
         entityManager.persist(questionEntity);
         return questionEntity;
+    }
+    /**
+     * Fetches a list of Question entities from the Database
+     *
+     * @return List of all questions
+     */
+    public List<QuestionEntity> getAllQuestions() {
+
+        List<QuestionEntity> questionsList =
+                entityManager.createNamedQuery("getAllQuestions", QuestionEntity.class).getResultList();
+        return questionsList;
     }
 }
